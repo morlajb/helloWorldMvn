25816f4ea718be8a72bb9cdcea179fe70105ee19
Patrik Behrami <x409062@nymfe43.fi.muni.cz>
2016-09-21 17:19:20 +0200

    first

---
+++ src/main/java/com/systems/app/App.java
@@ -0,0 +1,14 @@
+package com.systems.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+
+}
